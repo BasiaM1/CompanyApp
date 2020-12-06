cc7b1abe9eb763b46e11b1ade2be7f65fcb468e4
BasiaM1 <b.murzynowska@wp.pl>
2020-12-06 19:05:47 +0100

    Modify endpoints

--- server.js
+++ server.js
@@ -3,47 +3,48 @@ const cors = require('cors');
 const app = express();
 const mongoClient = require('mongodb').MongoClient;
 
-mongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
-  if (err) {
-    console.log(err);
-  }
-  else {
-    console.log('Successfully connected to the database');
-    const db = client.db('companyDB');
-    db.collection('employees').find({ department: 'IT' }).toArray((err, data) => {
-      if (!err) {
-        console.log(data)
-      }
-    });
-    db.collection('departments').insertOne({ name: 'Management' }, err => {
-      if (err) console.log('err');
-    });
-    // db.collection('employees').updateOne({ department: 'IT' }, { $set: { salary: 6000 } }, err => {
-    //   if (err) console.log(err);
-    // });
-    db.collection('employees').deleteOne({ salary: 6000 }, (err) => {
-      if (err) console.log(err);
-    });
-    app.use(cors());
-    app.use(express.json());
-    app.use(express.urlencoded({ extended: false }));
-    app.use((req, res, next) => {
-      req.db = db;
-      next();
-    });
-    app.use('/api', employeesRoutes);
-    app.use('/api', departmentsRoutes);
-    app.use('/api', productsRoutes);
+mongoClient.connect('mongodb://localhost:27017', {useNewUrlParser: true, useUnifiedTopology: true},
+  (err, client) => {
+    if (err) {
+      console.log(err);
+    }
+    else {
+      console.log('Successfully connected to the database');
+      const db = client.db('companyDB');
+      // db.collection('employees').find({ department: 'IT' }).toArray((err, data) => {
+      //   if (!err) {
+      //     console.log(data)
+      //   }
+      // });
+      // db.collection('departments').insertOne({ name: 'Management' }, err => {
+      //   if (err) console.log('err');
+      // });
+      // // db.collection('employees').updateOne({ department: 'IT' }, { $set: { salary: 6000 } }, err => {
+      // //   if (err) console.log(err);
+      // // });
+      // db.collection('employees').deleteOne({ salary: 6000 }, (err) => {
+      //   if (err) console.log(err);
+      // });
+      app.use(cors());
+      app.use(express.json());
+      app.use(express.urlencoded({extended: false}));
+      app.use((req, res, next) => {
+        req.db = db;
+        next();
+      });
+      app.use('/api', employeesRoutes);
+      app.use('/api', departmentsRoutes);
+      app.use('/api', productsRoutes);
 
-    app.use((req, res) => {
-      res.status(404).send({ message: 'Not found...' });
-    })
+      app.use((req, res) => {
+        res.status(404).send({message: 'Not found...'});
+      })
 
-    app.listen('8000', () => {
-      console.log('Server is running on port: 8000');
-    });
-  }
-});
+      app.listen('8000', () => {
+        console.log('Server is running on port: 8000');
+      });
+    }
+  });
 
 
 const employeesRoutes = require('./routes/employees.routes');
